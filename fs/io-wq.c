cd15f14a854ecdba4474acf8a38e9c1dcbbb4315
Jens Axboe <axboe@kernel.dk>
2021-02-26 10:20:34 -0700

    io-wq: fix double put of 'wq' in error path
    We are already freeing the wq struct here, so don't put it and get it
    freed twice.
    
    Reported-by: syzbot+7bf785eedca35ca05501@syzkaller.appspotmail.com
    Fixes: 4fb6ac326204 ("io-wq: improve manager/worker handling over exec")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io-wq.c
+++ fs/io-wq.c
@@ -1071,7 +1071,6 @@ struct io_wq *io_wq_create(unsigned bounded, struct io_wq_data *data)
 	if (!ret)
 		return wq;
 
-	io_wq_put(wq);
 	io_wq_put_hash(data->hash);
 err:
 	cpuhp_state_remove_instance_nocalls(io_wq_online, &wq->cpuhp_node);
