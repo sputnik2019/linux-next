24996dbdcee251790f865fee0d5214cff33688a6
Pavel Begunkov <asml.silence@gmail.com>
2021-03-24 22:59:01 +0000

    io_uring: reg buffer overflow checks hardening
    We are safe with overflows in io_sqe_buffer_register() because it will
    just yield alloc failure, but it's nicer to check explicitly.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/2b0625551be3d97b80a5fd21c8cd79dc1c91f0b5.1616624589.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -8384,6 +8384,8 @@ static int io_buffers_map_alloc(struct io_ring_ctx *ctx, unsigned int nr_args)
 
 static int io_buffer_validate(struct iovec *iov)
 {
+	unsigned long tmp, acct_len = iov->iov_len + (PAGE_SIZE - 1);
+
 	/*
 	 * Don't impose further limits on the size and buffer
 	 * constraints here, we'll -EINVAL later when IO is
@@ -8395,6 +8397,9 @@ static int io_buffer_validate(struct iovec *iov)
 	/* arbitrary limit, but we need something */
 	if (iov->iov_len > SZ_1G)
 		return -EFAULT;
+
+	if (check_add_overflow((unsigned long)iov->iov_base, acct_len, &tmp))
+		return -EOVERFLOW;
 
 	return 0;
 }
