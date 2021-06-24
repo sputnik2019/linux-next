7e6ea26bf1e998490dee1392a91999d342c4310a
Stephen Rothwell <sfr@canb.auug.org.au>
2021-06-24 18:50:59 +1000

    Revert "Merge branch 'marvell-mdio-ACPI'"
    This reverts commit 070258effa3b9603ac0cd6a40297b00a01ea5fd8, reversing
    changes made to 64295f0d01ae0661a2cea42c598070b1c87ca6e0.

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
@@ -4789,8 +4789,9 @@ static int mvpp2_open(struct net_device *dev)
 		goto err_cleanup_txqs;
 	}
 
-	if (port->phylink) {
-		err = phylink_fwnode_phy_connect(port->phylink, port->fwnode, 0);
+	/* Phylink isn't supported yet in ACPI mode */
+	if (port->of_node) {
+		err = phylink_of_phy_connect(port->phylink, port->of_node, 0);
 		if (err) {
 			netdev_err(port->dev, "could not attach PHY (%d)\n",
 				   err);
@@ -6698,19 +6699,6 @@ static void mvpp2_acpi_start(struct mvpp2_port *port)
 			  SPEED_UNKNOWN, DUPLEX_UNKNOWN, false, false);
 }
 
-/* In order to ensure backward compatibility for ACPI, check if the port
- * firmware node comprises the necessary description allowing to use phylink.
- */
-static bool mvpp2_use_acpi_compat_mode(struct fwnode_handle *port_fwnode)
-{
-	if (!is_acpi_node(port_fwnode))
-		return false;
-
-	return (!fwnode_property_present(port_fwnode, "phy-handle") &&
-		!fwnode_property_present(port_fwnode, "managed") &&
-		!fwnode_get_named_child_node(port_fwnode, "fixed-link"));
-}
-
 /* Ports initialization */
 static int mvpp2_port_probe(struct platform_device *pdev,
 			    struct fwnode_handle *port_fwnode,
@@ -6786,6 +6774,7 @@ static int mvpp2_port_probe(struct platform_device *pdev,
 	port = netdev_priv(dev);
 	port->dev = dev;
 	port->fwnode = port_fwnode;
+	port->has_phy = !!of_find_property(port_node, "phy", NULL);
 	port->ntxqs = ntxqs;
 	port->nrxqs = nrxqs;
 	port->priv = priv;
@@ -6928,7 +6917,8 @@ static int mvpp2_port_probe(struct platform_device *pdev,
 	dev->max_mtu = MVPP2_BM_JUMBO_PKT_SIZE;
 	dev->dev.of_node = port_node;
 
-	if (!mvpp2_use_acpi_compat_mode(port_fwnode)) {
+	/* Phylink isn't used w/ ACPI as of now */
+	if (port_node) {
 		port->phylink_config.dev = &dev->dev;
 		port->phylink_config.type = PHYLINK_NETDEV;
 
@@ -6940,7 +6930,6 @@ static int mvpp2_port_probe(struct platform_device *pdev,
 		}
 		port->phylink = phylink;
 	} else {
-		dev_warn(&pdev->dev, "Use link irqs for port#%d. FW update required\n", port->id);
 		port->phylink = NULL;
 	}
 
