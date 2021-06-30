576e1e9cf942ff2ffc800dad232374eab04cd906
Boris Burkov <boris@bur.io>
2021-06-30 13:01:49 -0700

    btrfs: initial fsverity support
    Add support for fsverity in btrfs. To support the generic interface in
    fs/verity, we add two new item types in the fs tree for inodes with
    verity enabled. One stores the per-file verity descriptor and btrfs
    verity item and the other stores the Merkle tree data itself.
    
    Verity checking is done in end_page_read just before a page is marked
    uptodate. This naturally handles a variety of edge cases like holes,
    preallocated extents, and inline extents. Some care needs to be taken to
    not try to verity pages past the end of the file, which are accessed by
    the generic buffered file reading code under some circumstances like
    reading to the end of the last page and trying to read again. Direct IO
    on a verity file falls back to buffered reads.
    
    Verity relies on PageChecked for the Merkle tree data itself to avoid
    re-walking up shared paths in the tree. For this reason, we need to
    cache the Merkle tree data. Since the file is immutable after verity is
    turned on, we can cache it at an index past EOF.
    
    Use the new inode ro_flags to store verity on the inode item, so that we
    can enable verity on a file, then rollback to an older kernel and still
    mount the file system and read the file. Since we can't safely write the
    file anymore without ruining the invariants of the Merkle tree, we mark
    a ro_compat flag on the file system when a file has verity enabled.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Co-developed-by: Chris Mason <clm@fb.com>
    Signed-off-by: Chris Mason <clm@fb.com>
    Signed-off-by: Boris Burkov <boris@bur.io>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -32,6 +32,7 @@
 #include <linux/sched/mm.h>
 #include <linux/iomap.h>
 #include <asm/unaligned.h>
+#include <linux/fsverity.h>
 #include "misc.h"
 #include "ctree.h"
 #include "disk-io.h"
@@ -5523,6 +5524,7 @@ void btrfs_evict_inode(struct inode *inode)
 	trace_btrfs_inode_evict(inode);
 
 	if (!root) {
+		fsverity_cleanup_inode(inode);
 		clear_inode(inode);
 		return;
 	}
@@ -5605,6 +5607,7 @@ no_delete:
 	 * to retry these periodically in the future.
 	 */
 	btrfs_remove_delayed_node(BTRFS_I(inode));
+	fsverity_cleanup_inode(inode);
 	clear_inode(inode);
 }
 
@@ -9176,6 +9179,7 @@ static int btrfs_getattr(struct user_namespace *mnt_userns,
 	struct inode *inode = d_inode(path->dentry);
 	u32 blocksize = inode->i_sb->s_blocksize;
 	u32 bi_flags = BTRFS_I(inode)->flags;
+	u32 bi_ro_flags = BTRFS_I(inode)->ro_flags;
 
 	stat->result_mask |= STATX_BTIME;
 	stat->btime.tv_sec = BTRFS_I(inode)->i_otime.tv_sec;
@@ -9188,6 +9192,8 @@ static int btrfs_getattr(struct user_namespace *mnt_userns,
 		stat->attributes |= STATX_ATTR_IMMUTABLE;
 	if (bi_flags & BTRFS_INODE_NODUMP)
 		stat->attributes |= STATX_ATTR_NODUMP;
+	if (bi_ro_flags & BTRFS_INODE_RO_VERITY)
+		stat->attributes |= STATX_ATTR_VERITY;
 
 	stat->attributes_mask |= (STATX_ATTR_APPEND |
 				  STATX_ATTR_COMPRESSED |
